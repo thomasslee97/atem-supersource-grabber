e10a492013094c7682136fad05c4c1e3659ffd55
Thomas Lee <thomasslee97@gmail.com>
2019-10-14 11:30:54 +0100

    fix: Write file sync

--- index.ts
+++ index.ts
@@ -11,8 +11,6 @@ conn.on('error', console.log)
 conn.connect(IP)
 conn.once('connected', () => {
     console.log(JSON.stringify(conn.state.video.superSources[0], undefined, 4))
-    fs.writeFile('state.json', JSON.stringify(conn.state.video.superSources[0]), (err) => {
-        if (err) throw err
-    })
+    fs.writeFileSync('state.json', JSON.stringify(conn.state.video.superSources[0]))
     process.exit(0)
 })
