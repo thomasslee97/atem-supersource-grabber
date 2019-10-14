0fc0c8b72be9b1a43c2814c86744e60bb3c41c77
Thomas Lee <thomasslee97@gmail.com>
2019-10-14 11:27:08 +0100

    feat: Write state to file

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { Atem } from 'atem-connection'
+import * as fs from 'fs'
 const IP = process.env.IP
 
 if (!IP) {
@@ -10,5 +11,8 @@ conn.on('error', console.log)
 conn.connect(IP)
 conn.once('connected', () => {
     console.log(JSON.stringify(conn.state.video.superSources[0], undefined, 4))
+    fs.writeFile('state.json', JSON.stringify(conn.state.video.superSources[0]), (err) => {
+        if (err) throw err
+    })
     process.exit(0)
 })
